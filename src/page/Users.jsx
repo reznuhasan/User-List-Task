40467259ef25ea6e551c51bd98c3a11e87235045
reznuhasan <reznuhasan31@gmail.com>
2024-02-10 18:54:02 +0600

    sorted by name,email,company name part add

--- src/page/Users.jsx
+++ src/page/Users.jsx
@@ -2,48 +2,96 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react"
 import UserCard from "../components/UserCard";
-import { Link } from "react-router-dom";
 
 const Users = () => {
     const [users, setUsers] = useState([]);
-    const [showUsers,setShowUsers]=useState([])
-    const [searchName,setSearchName]=useState("")
+    const [showUsers, setShowUsers] = useState([])
+    const [searchName, setSearchName] = useState("")
     useEffect(() => {
         const fetchData = async () => {
             const res = await axios.get(`https://dummyjson.com/users`);
             setUsers(res.data.users)
             setShowUsers(res.data.users)
+            const newArray = res.data.users
         }
         fetchData()
     }, [])
-    const searchByName=(e)=>{
-        const userName=e.target.value;
+    // ************** search part ************
+    const searchByName = (e) => {
+        const userName = e.target.value;
         setSearchName(e.target.value)
-        const findUser=users.filter(user=>user.firstName.toLowerCase().includes(userName.toLowerCase()))
-       setShowUsers(findUser)
+        const findUser = users.filter(user => user.firstName.toLowerCase().includes(userName.toLowerCase()))
+        setShowUsers(findUser)
     }
+    // ********sorted part start**************
+    const handleSelect=(e)=>{
+        // sorted by user name
+        if(e.target.value==='name'){
+            const namesortedArray=[...users].sort((a,b)=>{
+                const nameA=a.firstName.toLowerCase();
+                const nameB=b.firstName.toLowerCase();
+        
+                return nameA.localeCompare(nameB)    
+            })
+            setShowUsers(namesortedArray)
+        }
+        // sorted by email
+        else if(e.target.value==='email'){
+            const emailSortedArray=[...users].sort((a,b)=>{
+                const emailA=a.email.toLowerCase();
+                const emailB=b.email.toLowerCase();
+        
+                return emailA.localeCompare(emailB)    
+            })
+            setShowUsers(emailSortedArray)
+        }
+        // sorted by company name
+        else if(e.target.value==='company'){
+            const companySortedArray=[...users].sort((a,b)=>{
+                const companyA=a.company.name.toLowerCase();
+                const companyB=b.company.name.toLowerCase();
+        
+                return companyA.localeCompare(companyB)    
+            })
+            setShowUsers(companySortedArray)
+        }
+    }
+    // **************sorted part finish **************
     if (users.length === 0) return <h1>Loding..</h1>
     return (
         <div className="flex flex-col justify-center items-center">
+            {/* search input part */}
             <div>
-                <label htmlFor=""></label>
-                <input type="text" 
-                name="search" 
-                className="border-2 border-green-500 rounded-full p-2 placeholder-lime-800 mt-4 min-w-96 pl-4"  
-                placeholder="search user name"
-                onChange={searchByName}
+                <input type="text"
+                    name="search"
+                    className="border-2 border-green-500 rounded-full p-2 placeholder-lime-800 mt-4 min-w-96 pl-4"
+                    placeholder="search user name"
+                    onChange={searchByName}
                 />
             </div>
-            {
-                (showUsers.length === 0) ? <h1 className="mt-2 text-3xl text-red-700">User No Found</h1>:
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
-                {
-                    showUsers.map(user =><UserCard user={user} key={user.id} />)
-                }
+            {/* selected part */}
+            <div>
+                <select className="block w-full px-4 py-2 mt-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-500"
+                onChange={handleSelect}
+                >
+                    <option value="">Sorted User List</option>
+                    <option value="name">Name</option>
+                    <option value="email">Email</option>
+                    <option value="company">Company</option>
+                </select>
+            
             </div>
+            {/* user list part */}
+            {
+                (showUsers.length === 0) ? <h1 className="mt-2 text-3xl text-red-700">User No Found</h1> :
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
+                        {
+                            showUsers.map(user => <UserCard user={user} key={user.id} />)
+                        }
+                    </div>
 
             }
-            
+
         </div>
 
     )
